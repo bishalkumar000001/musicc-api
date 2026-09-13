fc2a6180a5bfaff773e4fc7254b32566ed35af84
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-09-13 19:08:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -972,7 +972,7 @@ def download_audio_sync(
         "extractor_args": {
 
             "youtube": [
-                "player_client=default,web_embedded"
+                "player_client=android_vr,web_embedded"
             ]
         },
 
@@ -1313,7 +1313,7 @@ def download_video_sync(
         "extractor_args": {
 
             "youtube": [
-                "player_client=default,web_embedded"
+                "player_client=android_vr,web_embedded"
             ]
         },
 
